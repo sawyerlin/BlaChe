9c95e05fc7bd0eea596fd56c27d1e24d370f023c
sawyerlin <sawyer.lin@gmail.com>
2016-07-09 21:40:03 +0200

    add components

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+})
+
+export class AppComponent { 
+    title = 'BlaChe';
+    hero = Hero = {
+        id: 1,
+        name: 'WindStorm'
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}
