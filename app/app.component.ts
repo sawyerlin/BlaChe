497fd5c35ea8d513b38d423c902dfcd21153989f
sawyerlin <sawyer.lin@gmail.com>
2016-07-11 14:14:17 +0200

    create hero class and finish hero detail component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
@@ -11,14 +27,8 @@ import { Component } from '@angular/core';
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
-    <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="name">
-        </div>
-    </div>`,
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
     styles: [`
         .selected {
             background-color: #CFD8DC !important;
@@ -67,34 +77,13 @@ import { Component } from '@angular/core';
             margin-right: .8em;
             border-radius: 4px 0 0 4px;
         }
-        `]
+        `],
+        directives: [HeroDetailComponent]
 })
 
 export class AppComponent { 
     title = 'BlaChe';
-    hero: Hero = {
-        id: 1,
-        name: 'WindStorm'
-    };
+    heroes = HEROES
     selectedHero: Hero;
     onSelect(hero: Hero) {this.selectedHero = hero;};
-    public heroes = HEROES
 }
-
-export class Hero {
-    id: number;
-    name: string;
-}
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
