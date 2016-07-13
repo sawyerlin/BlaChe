b8ada5ef6a000f2ce5381040e9c0d0db1041406c
sawyerlin <sawyer.lin@gmail.com>
2016-07-13 14:28:48 +0200

    add hero service and use promise to simulate the request from server

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
-    <h2>My Heros</h2>
+    <h2>My Cars</h2>
     <ul class="heroes">
         <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
@@ -78,12 +66,24 @@ const HEROES: Hero[] = [
             border-radius: 4px 0 0 4px;
         }
         `],
-        directives: [HeroDetailComponent]
+        directives: [HeroDetailComponent],
+        providers: [HeroService]
 })
 
-export class AppComponent { 
+export class AppComponent implements OnInit {
     title = 'BlaChe';
-    heroes = HEROES
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService) {}
+
+    getHeroes() {
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
     onSelect(hero: Hero) {this.selectedHero = hero;};
 }
